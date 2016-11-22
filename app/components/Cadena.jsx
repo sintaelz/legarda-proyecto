977aabdd4f264e3b8f89c26c589f663123219f43
Seung Lee <sintaelz@gmail.com>
2016-11-21 23:01:15 -0600

    agregar

---
+++ app/components/Cadena.jsx
@@ -0,0 +1,11 @@
+const React = require('React');
+
+var Cadena = React.createClass({
+  render: function() {
+    return (
+      <h1>Cadena Component</h1>
+    );
+  }
+});
+
+export default Cadena;
